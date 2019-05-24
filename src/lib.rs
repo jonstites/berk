b26760632e4f5297a5c768d7f327a773a6fa7138
Jonathan Stites <mail@jonstites.com>
2019-05-24 17:25:16 -0500

    Write hash-objects with zlib compression

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,21 @@ use std::fmt;
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use std::result;
+
+pub type Result<T> = result::Result<T, BerkError>;
+
+#[derive(Debug)]
+pub enum BerkError {
+    NotAGitRepo,
+    IOError(std::io::Error),
+}
+
+impl From<std::io::Error> for BerkError {
+    fn from(err: std::io::Error) -> BerkError {
+        BerkError::IOError(err)
+    }
+}
 
 pub enum ObjectType {
     Commit,
@@ -13,14 +28,15 @@ pub enum ObjectType {
 
 pub struct Object {
     object_type: ObjectType,
-    contents: Vec<u8>,    
+    contents: Vec<u8>,
 }
 
 impl Object {
-
     pub fn with_header(&self) -> Vec<u8> {
         let mut v = Vec::new();
-        v.extend_from_slice(format!("{} {}\0", self.object_type, self.contents.len().to_string()).as_bytes());
+        v.extend_from_slice(
+            format!("{} {}\0", self.object_type, self.contents.len().to_string()).as_bytes(),
+        );
         v.extend_from_slice(&self.contents);
         v
     }
@@ -43,24 +59,34 @@ pub fn object_from_file(filename: &str) -> std::io::Result<Object> {
     file.read_to_end(&mut contents)?;
 
     let object_type = ObjectType::Blob;
-    Ok(Object{object_type, contents })
+    Ok(Object {
+        object_type,
+        contents,
+    })
 }
 
 pub fn hash_object(object: &Object) -> String {
-    let hash = Sha1::new()
-        .chain(object.with_header())
-        .result();
+    let hash = Sha1::new().chain(object.with_header()).result();
 
     format!("{:x}", hash)
 }
 
-/*pub fn is_git_directory(path: Path) -> bool {
-    true
+pub fn is_git_src(path: &Path) -> bool {
+    // TODO: there is supposed to be a check that HEAD is valid
+    path.join(".git/HEAD").is_file()
+        && path.join(".git/objects").is_dir()
+        && path.join(".git/refs").is_dir()
 }
 
-pub fn find_git_src(path: Path) -> Result<Path, ()> {
-    Ok(Path::new())
-}*/
+pub fn find_git_src(path: &Path) -> Result<&Path> {
+    let ancestors = path.ancestors();
+    for ancestor in ancestors {
+        if is_git_src(ancestor) {
+            return Ok(ancestor);
+        }
+    }
+    Err(BerkError::NotAGitRepo)
+}
 
 #[cfg(test)]
 mod tests {
@@ -71,7 +97,10 @@ mod tests {
     fn test_hash_object() {
         let contents = "what is up, doc?".as_bytes().to_vec();
         let object_type = ObjectType::Blob;
-        let object = Object{contents, object_type};
+        let object = Object {
+            contents,
+            object_type,
+        };
         let hash = hash_object(&object);
         assert_eq!(hash, "bd9dbf5aae1a3862dd1526723246b20206e5fc37");
     }
--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,48 @@ extern crate berk;
 #[macro_use]
 extern crate clap;
 use clap::{App, Arg, SubCommand};
+use std::path::Path;
 
-fn main() -> std::io::Result<()> {
+use flate2::Compression;
+use flate2::write::ZlibEncoder;
+use std::fs::{File, create_dir_all};
+
+use std::io::prelude::*;
+
+fn main() -> berk::Result<()> {
     let matches = App::new("berk")
         .version(crate_version!())
         .about("A git implementation for no good reason")
-        .subcommand(SubCommand::with_name("hash-object")
-            .arg(Arg::with_name("file").index(1))
-            .arg(Arg::with_name("write").short("w").help("Actually write the object into the object database.")))
+        .subcommand(
+            SubCommand::with_name("hash-object")
+                .arg(Arg::with_name("file").index(1))
+                .arg(
+                    Arg::with_name("write")
+                        .short("w")
+                        .help("Actually write the object into the object database."),
+                ),
+        )
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("hash-object") {
         if let Some(filename) = matches.value_of("file") {
+
             let object = berk::object_from_file(&filename)?;
             let hash = berk::hash_object(&object);
-            
+
             if matches.is_present("write") {
-                
+                let path = Path::new(".");
+                let git_src = berk::find_git_src(&path)?;
+                let object_dest = git_src.join(".git/objects").join(hash[..2].to_string()).join(hash[2..].to_string());
+
+                if let Some(parent) = object_dest.parent() {
+                    create_dir_all(parent)?;
+                }
+                let file = File::create(object_dest)?;
+
+                let mut e = ZlibEncoder::new(file, Compression::default());
+                e.write_all(&object.with_header())?;
+                e.flush_finish()?;
             }
             println!("{}", hash)
         }
